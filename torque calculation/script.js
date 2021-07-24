cdd708138f65fb77dd68a709bb58069aa4bbb84e
renugab <renugasri@gmail.com>
2021-07-24 19:33:44 +0530

    torque

---
+++ torque calculation/script.js
@@ -0,0 +1,8 @@
+function multiplyBy()
+{
+        num1 = document.getElementById("Distance").value;
+        num2 = document.getElementById("Force").value;
+        num3 = document.getElementById("Angle").value;
+        document.getElementById("result").innerHTML = num1*num2*num3;
+}
+
